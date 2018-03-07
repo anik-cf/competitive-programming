2fccf78df7b1f37078a50907340bd5c9f952cd56
Anik Kumar Chakrabortty <hello@anik-online.com>
2018-03-07 13:58:13 +0600

    Educational Codeforces Round 39

---
+++ codeforces/946A - Partition.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   
+
+   	int n, x=0, y=0; 
+   	cin >> n; 
+ 
+   	for(int i=0; i<n; i++){
+   	int a; 
+   	cin >> a; 
+   	if(a<0){
+   		y+=a; 
+	}
+	
+	else
+   	x += a; 
+}
+	   
+	   cout << x - y << endl; 
+	
+}
