a4d7678c45cbed6bdac7d8629693dd12654678d6
Anik Kumar Chakrabortty <hello@anik-online.com>
2018-12-31 03:10:38 +0600

    Good Bye 2018

---
+++ codeforces/1091A - New Year and the Christmas Ornament.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h> 
+#define ll long long
+#define ull unsigned long long
+#define MAX 10000007
+#define PI acos(-1)
+#define pb(x) push_back(x)
+#define inpll(a) scanf("%lld",&a)
+#define vi vector<int>
+#define READ(f) freopen(f, "r", stdin)
+#define WRITE(f) freopen(f, "w", stdout)
+ 
+/*
+	Anik Chakrabortty
+	Southeast University 
+	Dhaka Bangladesh
+	Preparation Set Fall 2018
+
+*/ 
+
+
+using namespace std;
+ 
+
+int main()
+{
+   
+    //READ("in.txt");
+    //WRITE("out.txt");
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int a, b, c; 
+    cin >> a >> b >> c; 
+
+    int del = min(a, b-1); 
+    del = min(del, c-2); 
+
+    cout << 3*(del+1) << endl;
+
+
+    return 0; 
+}
